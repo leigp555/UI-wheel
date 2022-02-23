648c56863bba448715d675c0c9107e724173ebed
leigp <907090585@qq.com>
2022-02-24 05:29:04 +0800

    完成button组件

--- src/components/buttonEg.vue
+++ src/components/buttonEg.vue
@@ -1,15 +1,35 @@
 <template>
-button
+  <ol>
+    <li>
+      <Button>直角</Button>
+    </li>
+    <li>
+      <Button circle>圆角</Button>
+    </li>
+    <li>
+      <Button disabled>禁用</Button>
+    </li>
+    <li>
+      <Button size="big">大按钮</Button>
+    </li>
+    <li>
+      <Button size="small">小按钮</Button>
+    </li>
+  </ol>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
-
+import Button from "../lib/Button.vue";
 export default defineComponent({
-  name: "Button"
+  components: {Button}
 })
 </script>
 
 <style lang="scss" scoped>
-
+ol{
+  li {
+    margin-bottom: 10px;
+  }
+}
 </style>
---
+++ src/lib/Button.vue
@@ -0,0 +1,62 @@
+<template>
+  <button class="gulu-button"
+          :class="`gulu-button-${size}`,{
+        'gulu-button-circle':circle,
+        'gulu-button-disabled':disabled,
+    }">
+    <slot/>
+  </button>
+</template>
+
+<script lang="ts">
+import {defineComponent, toRefs} from "vue";
+
+export default defineComponent({
+  name: "Button",
+  props: {
+    circle: Boolean,
+    disabled: Boolean,
+    size: {
+      type: String,
+      default: "normal"
+    }
+  },
+  setup(props) {
+    const {circle, disabled, size} = toRefs(props)
+    return {circle, disabled, size}
+  }
+})
+</script>
+
+
+<style lang="scss" scoped>
+.gulu-button {
+  padding: 5px 20px;
+  background-color: #ffffff;
+  border: none;
+  border-radius: 4px;
+  text-align: center;
+  white-space: nowrap;
+  box-shadow: 0 0 2px 0 rgba(0, 0, 0, .6);
+
+  &:hover {
+    background-color: #ecf5ff;
+  }
+
+  &-circle {
+    border-radius: 15px;
+  }
+
+  &-disabled {
+    cursor: not-allowed;
+  }
+  &-big{
+    padding: 5px 25px;
+    font-size: 18px;
+  }
+  &-small {
+    padding: 5px 15px;
+    font-size: 14px;
+  }
+}
+</style>
