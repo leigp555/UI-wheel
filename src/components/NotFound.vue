d076109b47034b6b8b5b714ee8c35c7229b845bd
leigp <907090585@qq.com>
2022-02-23 18:07:57 +0800

    添加404路由

---
+++ src/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>404</div>
+<div>无效的路径</div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+export default defineComponent({
+  name: "NotFound"
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,15 +1,16 @@
-import {createRouter,createWebHashHistory} from 'vue-router'
+import {createRouter, createWebHashHistory} from 'vue-router'
 import Doc from "../components/Doc.vue";
 import Home from "../components/Home.vue";
+import NotFound from "../components/NotFound.vue";
 
 
-
-const history=createWebHashHistory()
-export const router=createRouter({
-    history:history,
-    routes:[
-        {path:"/",component:Home},
-        {path:"/home",component:Home},
-        {path:"/doc",component:Doc},
+const history = createWebHashHistory()
+export const router = createRouter({
+    history: history,
+    routes: [
+        {path: "/", component: Home},
+        {path: "/home", component: Home},
+        {path: "/doc", component: Doc},
+        {path: '/:pathMatch(.*)*', component: NotFound},
     ]
 })
