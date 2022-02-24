d50c14ee6b25353b31b33a16d20444ad0f4b6e20
leigp <907090585@qq.com>
2022-02-25 02:49:49 +0800

    填写tab组件使用说明

--- src/components/tabEg.vue
+++ src/components/tabEg.vue
@@ -1,14 +1,14 @@
 <template>
   <Show v-model:attribute="attribute">
     <template v-slot:title>
-      <h1>Switch 开关</h1>
-      <h4>表示两种相互对立的状态间的切换，多用于触发「开/关」。</h4>
+      <h1>Tabs 标签页</h1>
+      <h4>分隔内容上有关联但属于不同类别的数据集合。</h4>
     </template>
     <template v-slot:useAge>
       <UseAge>
         <template v-slot:title>
           <h2>基础用法</h2>
-          <h4>绑定 v-model 到一个 Boolean 类型的变量。 可以使用 active-color 属性与 inactive-color 属性来设置开关的背景色</h4>
+          <h4>Tabs 组件提供了选项卡功能， 默认选中第一个标签页，你也可以通过 value 属性来指定当前选中的标签页。</h4>
         </template>
         <template v-slot:code></template>
         <template v-slot:example>
@@ -31,10 +31,9 @@ import UseAge from "./UseAge.vue";
 
 const selected = ref("标题一")
 const attribute=ref([
-  {attribute:"loading",tip:"是否显示加载中xxxxxxxxxxxxx",type:"boolean",options:"large",default:"10"},
-  {attribute:"loading",tip:"是否显示加载中",type:"boolean",options:"large",default:"10"},
-  {attribute:"loading",tip:"是否显示加载中",type:"boolean",options:"large",default:"10"},
-  {attribute:"loading",tip:"是否显示加载中",type:"boolean",options:"large",default:"10"}
+  {attribute:"selected",tip:"绑定值，值为所设置的插槽中的title",type:"string",options:"插槽内的title",default:""},
+  {attribute:"Tab组件",tip:"必须结合Tab组件使用",type:"组件",options:"",default:""},
+  {attribute:"title",tip:"Tab中必须传title属性",type:"string",options:"",default:""}
 ])
 </script>
 
