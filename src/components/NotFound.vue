8f682b0b3211b36690860b578da90b4879668cd5
leigp <907090585@qq.com>
2022-02-28 02:35:12 +0800

    美化404页面

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>404</div>
-<div>无效的路径</div>
+  <div class="notFound">
+    <div class="inner">
+      <video width="200" autoplay loop muted>
+        <source src="../../public/video.mp4" type="video/mp4"/>
+      </video>
+      <p>无法访问此网站</p>
+      <p>检查网址中是否有拼写错误。</p>
+      <p>如果拼写无误，请尝试运行 Windows 网络诊断。</p>
+      <p>DNS_PROBE_FINISHED_NXDOMAIN</p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -12,5 +21,21 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+.notFound {
+  position: relative;
+  > .inner {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translateX(-50%)translateY(50%);
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+    font-family: 'Segoe UI', Arial, 'Microsoft Yahei', sans-serif;
+    >video{
+      margin-bottom:10px;
+    }
+  }
 
+}
 </style>
